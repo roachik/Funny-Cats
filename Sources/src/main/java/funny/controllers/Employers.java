d1645099c97627b31f0cf2bc66ff8b646f079809
Tatiana <shvtati@gmail.com>
2016-05-21 20:51:26 +0300

    bi fixed

--- Sources/src/main/java/funny/controllers/Employers.java
+++ Sources/src/main/java/funny/controllers/Employers.java
@@ -2,7 +2,10 @@ package funny.controllers;
 
 import funny.Base;
 import funny.HibernateSessionFactory;
-import funny.entity.*;
+import funny.entity.Employer;
+import funny.entity.EmployersOfStaffs;
+import funny.entity.Position;
+import funny.entity.Schedule;
 import funny.models.*;
 import org.hibernate.Session;
 import org.springframework.stereotype.Controller;
@@ -12,7 +15,6 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import java.sql.SQLException;
-import java.util.ArrayList;
 import java.util.List;
 
 /**
@@ -26,29 +28,14 @@ public class Employers extends Base {
                                    @RequestParam(value="pos",required = true) String pos)
             throws SQLException {
         putModel(model);
-        Department d = ModelDepartments.getDepartment(Integer.parseInt(dep));
-        Position p = ModelPositions.getPosition(Integer.parseInt(pos));
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/departments\">Отделы</a>");
-        breads.add("<a href=\"/departments?id="+dep+"\">"+d.getName()+"</a>");
-        breads.add("<a href=\"/schedules?id="+dep+"\">Ставки</a>");
-        breads.add("<a href=\"/employersofstaffs?dep="+dep+"&pos="+pos+"\">"+p.getName()+"</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
         List<EmployersOfStaffs> list = ModelEmployers.getEmployersOfStaffs(Integer.parseInt(dep),Integer.parseInt(pos));
         model.addAttribute("table", list);
         Employer e = (Employer) getSession().getAttribute("emp");
-        int role = ModelMain.getRoleDep(Integer.parseInt(dep), e.getEmployerId());
-        if(role == -1){
-            model.addAttribute("role",0);
-        }
-        else model.addAttribute("role",role);
-
-        /*if((ModelMain.getRole(e.getEmployerId())==2)&&(ModelMain.getRoleDep(Integer.parseInt(dep), e.getEmployerId())==-1)) {
+        if((ModelMain.getRole(e.getEmployerId())==2)&&(ModelMain.getRoleDep(Integer.parseInt(dep), e.getEmployerId())==-1)) {
             model.addAttribute("role", 0);
         } else {
             model.addAttribute("role", ModelMain.getRole(e.getEmployerId()));
-        }*/
+        }
         return "employersofstaffs";
     }
 
@@ -62,32 +49,13 @@ public class Employers extends Base {
     @RequestMapping("/employersofstaffs/add")
     public String employersofstaffadd(Model model,@RequestParam(value="part",required = false) String part,
                                       @RequestParam(value="emp",required = false) String emp,
-                                      @RequestParam(value="pos",required = false) String pos,
-                                      @RequestParam(value="dep",required = false) String dep) throws SQLException {
+                                      @RequestParam(value="pos",required = true) String pos,
+                                      @RequestParam(value="dep",required = true) String dep) throws SQLException {
         if(pos != null && emp != null && dep != null && part != null){
             ModelEmployers.addStaff(Integer.parseInt(dep),Integer.parseInt(pos),Integer.parseInt(emp),Double.parseDouble(part));
             return "redirect:/employersofstaffs/?dep="+dep+"&pos="+pos;
         }
-        putModel(model);
-        Department d = null;
-        Position p = null;
-        Employer e = null;
-        if(dep != null) d = ModelDepartments.getDepartment(Integer.parseInt(dep));
-        if(pos != null) p = ModelPositions.getPosition(Integer.parseInt(pos));
-        if(emp != null) e = ModelEmployers.getEmployer(Integer.parseInt(emp));
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/departments\">Отделы</a>");
-        if(dep!=null && pos != null){
-            breads.add("<a href=\"/departments?id="+dep+"\">"+d.getName()+"</a>");
-            breads.add("<a href=\"/schedules?id="+dep+"\">Ставки</a>");
-            breads.add("<a href=\"/employersofstaffs?dep="+dep+"&pos="+pos+"\">"+p.getName()+"</a>");
-        }
-        breads.add("<a href=\"#\">Новая запись</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
         model.addAttribute("Emps", ModelEmployers.getEmployers());
-        model.addAttribute("Poss", ModelPositions.getPositions());
-        model.addAttribute("Deps", ModelDepartments.getDepartmentsAll());
         return "add_employerofstaff";
     }
 
@@ -95,21 +63,15 @@ public class Employers extends Base {
     public String employersofstaffedit(Model model, @RequestParam(value="part",required = false) String part,
                                        @RequestParam(value="id",required = true) String id,
                                        @RequestParam(value="active",required = false) String active) throws SQLException {
+       System.out.println(Integer.parseInt(id));
+       // System.out.println(part);
 
+       // System.out.println(Integer.parseInt(active));
         putModel(model);
         EmployersOfStaffs e = ModelEmployers.getEmployerOfStaff(Integer.parseInt(id));
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/departments\">Отделы</a>");
-        breads.add("<a href=\"/departments?id="+e.getDepartment().getDepartmentId()+"\">"+e.getDepartment().getName()+"</a>");
-        breads.add("<a href=\"/schedules?id="+e.getDepartment().getDepartmentId()+"\">Ставки</a>");
-        breads.add("<a href=\"/employersofstaffs?dep="+e.getDepartment().getDepartmentId()+"&pos="+e.getPosition().getPositionId()+"\">"+e.getPosition().getName()+"</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
         model.addAttribute("info", e);
         Employer em = (Employer) getSession().getAttribute("emp");
-        if(active != null){
-            ModelEmployers.activate(Integer.parseInt(id));
-        }
+
         if(part != null){
             System.out.println(part);
             if(active != null) {
@@ -119,20 +81,12 @@ public class Employers extends Base {
 
             }
         }
-        model.addAttribute("Emps", ModelEmployers.getEmployers());
-        model.addAttribute("Poss", ModelPositions.getPositions());
-        model.addAttribute("Deps", ModelDepartments.getDepartmentsAll());
-        int role = ModelMain.getRoleDep(e.getDepartment().getDepartmentId(), em.getEmployerId());
-        if(role == -1){
-            model.addAttribute("role",0);
-        }
-        else model.addAttribute("role",role);
 
-        /*if((ModelMain.getRole(em.getEmployerId())==2)&&(ModelMain.getRoleDep(e.getDepartment().getDepartmentId(), em.getEmployerId())==-1)) {
+        if((ModelMain.getRole(em.getEmployerId())==2)&&(ModelMain.getRoleDep(e.getDepartment().getDepartmentId(), em.getEmployerId())==-1)) {
             model.addAttribute("role", 0);
         } else {
             model.addAttribute("role", ModelMain.getRole(em.getEmployerId()));
-        }*/
+        }
         if(part!=null){
             return "redirect:/";
         } else {
@@ -145,19 +99,10 @@ public class Employers extends Base {
     public String employers(Model model)
             throws SQLException {
         putModel(model);
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/employers\">Сотрудники</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
         List<Employer> list = ModelEmployers.getEmployers();
         model.addAttribute("table", list);
         Employer e = (Employer) getSession().getAttribute("emp");
-
-        int role = ModelMain.getRole(e.getEmployerId());
-        if(role == -1){
-            model.addAttribute("role",0);
-        }
-        else model.addAttribute("role",role);
+        model.addAttribute("role", ModelMain.getRole(e.getEmployerId()));
         return "employers";
     }
 
@@ -165,7 +110,7 @@ public class Employers extends Base {
     public String delete(Model model,@RequestParam(value="id",required = true) String id)
             throws SQLException {
         ModelSchedules.deleteById(Employer.class, Integer.parseInt(id));
-        return "redirect:/employers";
+        return "redirect:/";
     }
 
     @RequestMapping("/employers/add")
@@ -174,12 +119,6 @@ public class Employers extends Base {
             ModelEmployers.addEmployer(name);
             return "redirect:/employers";
         }
-        putModel(model);
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/employers\">Сотрудники</a>");
-        breads.add("<a href=\"/employers/add\">Новый сотрудник</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
         return "add_employer";
     }
 
@@ -188,15 +127,8 @@ public class Employers extends Base {
         if(name != null){
             ModelEmployers.updateEmployer(Integer.parseInt(id),name);
         }
-
         putModel(model);
-        Employer e = ModelEmployers.getEmployer(Integer.parseInt(id));
-        model.addAttribute("info", e);
-        ArrayList<String> breads = new ArrayList<String>();
-        breads.add("<a href=\"/\">Главная</a>");
-        breads.add("<a href=\"/employers\">Сотрудники</a>");
-        breads.add("<a href=\"/employers/edit?id="+id+"\">"+e.getName()+"</a>");
-        model.addAttribute("breadcrumbs",getBreadcrumbs(breads));
+        model.addAttribute("info", ModelEmployers.getEmployer(Integer.parseInt(id)));
         return "edit_employer";
     }
 
